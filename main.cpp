90e4384bafecd1379a2f5c4a08824d40ba64ab35
Anna Szybalska <anula.szy@gmail.com>
2013-11-24 21:25:29 +0100

    Modifing main to start actual app, not test.

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    //TChat viewer;
-    XmppTest viewer;
+    TChat viewer;
+    //XmppTest viewer;
     viewer.setOrientation(Html5ApplicationViewer::ScreenOrientationAuto);
     viewer.resize(600, 500);
     viewer.showExpanded();
 
-    //viewer.loadFile(QLatin1String("html/index.html"));
-    viewer.loadFile(QLatin1String("html/xmpptest.html"));
+    viewer.loadFile(QLatin1String("html/index.html"));
+    //viewer.loadFile(QLatin1String("html/xmpptest.html"));
 
     return app.exec();
 }
